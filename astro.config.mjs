e8072dd6a031afca74775db45309aaf6289935d7
meaningfool <josselin.perrus@gmail.com>
2026-01-07 10:39:32 +0100

    feat: enable hard line breaks in markdown with remark-breaks

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import remarkBreaks from 'remark-breaks';
 
 // https://astro.build/config
 export default defineConfig({
+    markdown: {
+        remarkPlugins: [remarkBreaks],
+    },
     integrations: [sitemap()],
     site: 'https://meaningfool.net',
     base: '/',
